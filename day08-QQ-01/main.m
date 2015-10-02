a88a730e9617bb2487adc1663988c189271766bf
lizhen <lizhen@lady8844.com>
2015-10-02 10:39:39 +0800

    Initial Commit

---
+++ day08-QQ-01/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  day08-QQ-01
+//
+//  Created by leon on 15/10/2.
+//  Copyright © 2015年 leon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ day08-QQ-01Tests/day08_QQ_01Tests.m
@@ -0,0 +1,39 @@
+//
+//  day08_QQ_01Tests.m
+//  day08-QQ-01Tests
+//
+//  Created by leon on 15/10/2.
+//  Copyright © 2015年 leon. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface day08_QQ_01Tests : XCTestCase
+
+@end
+
+@implementation day08_QQ_01Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
